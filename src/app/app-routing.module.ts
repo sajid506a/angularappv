37ed4f15e8634e3409f3d9e7b5b646de91464ef4
imran <sajid506a@gmail.com>
2022-11-12 23:24:16 +0530

    added products and contacts modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
   Shell.childRoutes([
-    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
+    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+    { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+    { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) },
+  
   ]),
-  // Fallback when no prior route is matched
+   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
